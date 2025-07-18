6a16d3ab94fe334203a007fe12f6b4fa53c2d307
caioau012 <caio_hudak@hotmail.com>
2025-07-17 21:23:53 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
